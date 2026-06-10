55b59f82ccce1648fe086f7636adaf62f3f8244e
Zahir Sanchez <emmsa0206@gmail.com>
2026-06-09 20:11:43 -0500

    feat: completed infrastructure layer

--- MineDev.MineTrack.Platform/shared/Infrastructure/Interfaces/AspNetCore/Configuration/KebabCaseRouteNamingConvention.cs
+++ MineDev.MineTrack.Platform/shared/Infrastructure/Interfaces/AspNetCore/Configuration/KebabCaseRouteNamingConvention.cs
@@ -1,14 +1,17 @@
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using MineDev.MineTrack.Platform.Shared.Infrastructure.Interfaces.AspNetCore.Configuration.Extensions;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace MineDev.MineTrack.Platform.Shared.Infrastructure.Interfaces.AspNetCore.Configuration;
 
 /// <summary>
-///     Replaces [controller] tokens in route templates with kebab-case.
-///     Example: MachineryController → /api/v1/machinery
+///     This class is used to replace the default route naming convention with a kebab-case naming convention.
 /// </summary>
 public class KebabCaseRouteNamingConvention : IControllerModelConvention
 {
+    /// <summary>
+    ///     This method applies the kebab-case naming convention to the controller.
+    /// </summary>
+    /// <param name="controller">The <see cref="ControllerModel" /></param>
     public void Apply(ControllerModel controller)
     {
         foreach (var selector in controller.Selectors)
@@ -18,6 +21,12 @@ public class KebabCaseRouteNamingConvention : IControllerModelConvention
             selector.AttributeRouteModel = ReplaceControllerTemplate(selector, controller.ControllerName);
     }
 
+    /// <summary>
+    ///     This method replaces the default controller template with a kebab-case template.
+    /// </summary>
+    /// <param name="selector">The <see cref="SelectorModel" /></param>
+    /// <param name="name">string containing the name</param>
+    /// <returns></returns>
     private static AttributeRouteModel? ReplaceControllerTemplate(SelectorModel selector, string name)
     {
         return selector.AttributeRouteModel != null
