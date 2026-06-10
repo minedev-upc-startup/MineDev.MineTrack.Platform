f86dd33f37d3d8b70144963a99e264a6a5fa9fad
sebastian <saiquipapoma@gmail.com>
2026-06-09 19:40:09 -0500

    feat(iam): add IamError enum

---
+++ MineDev.MineTrack.Platform/Iam/Domain/Model/IamError.cs
@@ -0,0 +1,10 @@
+namespace MineDev.MineTrack.Platform.Iam.Domain.Model;
+
+public enum IamError
+{
+    InvalidCredentials,
+    UsernameAlreadyExists,
+    UserNotFound,
+    InvalidToken,
+    Unauthorized
+}
