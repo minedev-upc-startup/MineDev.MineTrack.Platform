124005ea430170ef0646db290b8397a7cefb77ed
Juan José <juanjoelnoob2003@gmail.com>
2026-07-07 19:48:36 -0500

    fix: Remove XML documentation from StringExtensions

--- MineDev.MineTrack.Platform/Shared/Infrastructure/Interfaces/AspNetCore/Configuration/Extensions/StringExtensions.cs
+++ MineDev.MineTrack.Platform/Shared/Infrastructure/Interfaces/AspNetCore/Configuration/Extensions/StringExtensions.cs
@@ -1,19 +1,8 @@
 using System.Text.RegularExpressions;
 
 namespace MineDev.MineTrack.Platform.Shared.Infrastructure.Interfaces.AspNetCore.Configuration.Extensions;
-
-/// <summary>
-///     Extension methods for <see cref="string" />.
-/// </summary>
 public static partial class StringExtensions
 {
-    /// <summary>
-    ///     Converts the text to kebab case.
-    /// </summary>
-    /// <param name="text">string to convert</param>
-    /// <returns>
-    ///     The kebab case string.
-    /// </returns>
     public static string ToKebabCase(this string text)
     {
         if (string.IsNullOrEmpty(text))
